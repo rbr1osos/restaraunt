7720f5dadac070d9617ed348576c9133a67dbd47
Ryan Briosos <rybri415@yahoo.com>
2023-03-15 16:18:09 -0700

    finished footer

--- src/modules/initial-page-load.js
+++ src/modules/initial-page-load.js
@@ -75,9 +75,42 @@ function footerContent(){
     const footer = document.createElement('footer');
     footer.classList.add('footer');
 
+    //footer text
     const copyright = document.createElement('p');
     copyright.textContent=`Copyright © ${new Date().getFullYear()} rbr1osos`
     footer.appendChild(copyright)
+
+
+    //social links container
+    const logo_container = document.createElement('div')
+    logo_container.classList.add('logo-container');
+
+    //social media links
+    const anchor=document.createElement('a')
+    anchor.href='https://github.com/rbr1osos';
+    const logo_git = document.createElement('img');
+    logo_git.classList.add('social_logo');
+    logo_git.src='./images/github.png'
+    anchor.appendChild(logo_git);
+    logo_container.appendChild(anchor);
+
+    const anchor_behance=document.createElement('a')
+    anchor_behance.href='https://behance.net/ryanbriosos';
+    const logo_behance = document.createElement('img');
+    logo_behance.classList.add('social_logo');
+    logo_behance.src='./images/behance.png'
+    anchor_behance.appendChild(logo_behance);
+    logo_container.appendChild(anchor_behance);
+
+    const anchor_linked=document.createElement('a')
+    anchor_linked.href='https://linkedin.com/in/ray-ryan-briosos/';
+    const logo_linked = document.createElement('img');
+    logo_linked.classList.add('social_logo');
+    logo_linked.src='./images/linked-in.png'
+    anchor_linked.appendChild(logo_linked);
+    logo_container.appendChild(anchor_linked);
+
+    footer.appendChild(logo_container);
     return footer;
 }
 
