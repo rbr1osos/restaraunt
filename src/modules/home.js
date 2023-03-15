c867204df0197479496ddc7d4a886d5c257ebaeb
Ryan Briosos <rybri415@yahoo.com>
2023-03-15 03:44:21 -0700

    checking

--- src/modules/home.js
+++ src/modules/home.js
@@ -9,7 +9,7 @@ function createHome(){
 
     const title_h2 = document.createElement('h2');
     title_h2.classList.add('header-h2');
-    title_h2.textContent=`CHEF'S SPECIAL`;
+    title_h2.textContent=`---CHEF'S SPECIAL`;
 
     const title_h1 = document.createElement('h1');
     title_h1.classList.add('header-h1');
@@ -26,7 +26,7 @@ function createHome(){
     //create hero image
     const hero_image = document.createElement('img');
     hero_image.classList.add('hero-image');
-    hero_image.src='../dist/images/hero.png';
+    hero_image.src='./images/hero.png';
 
     left_content.appendChild(title_h2)
     left_content.appendChild(title_h1)
